1e8637063bc3f53929223501efcd834343754187
mudit_1711 <muditagrawal06@gmail.com>
2026-07-28 19:53:09 +0530

    fence.cpp

--- fence.cpp
+++ fence.cpp
@@ -27,7 +27,29 @@
 #include <bitset>
 using namespace std;
 int main() {
-    
-
+    int i,j,n,k;
+    cin>>n>>k;
+    vector<int>h(n);
+    for(i=0;i<n;i++){
+        cin>>h[i];
+    }
+    vector<int>pref(n,0);
+    pref[0]=h[0];
+    for(i=1;i<n;i++){
+        pref[i]=h[i]+pref[i-1];
+    }
+    int ans=-1;
+    int mn=INT_MAX;
+    for(i=0;i<=n-k;i++){
+        int sum = pref[i+k-1]-pref[i-1];
+        if(i==0){
+            sum=pref[k-1];
+        }
+        if(sum<mn){
+            ans=i+1;
+            mn=sum;
+        }
+    }
+    cout<<ans;
     return 0;
 }
